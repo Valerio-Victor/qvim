aaf31ad7be5f155b8763dc7c1054f1ff6cbe31b5
Victor Valerio <victor.dmv@unifei.edu.br>
2024-09-10 18:24:04 -0300

    Atualização.

---
+++ scripts/analises.R
@@ -0,0 +1,232 @@
+# PACOTES: ----------------------------------------------------------------
+library(magrittr)
+library(ggplot2)
+
+
+
+
+# IMPORTAÇÃO E ARRUMAÇÃO: -------------------------------------------------
+base <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                            sheet = 2,
+                            range = 'A1:AE5395') %>%
+  janitor::clean_names() %>%
+  tidyr::separate(col = 'hierarquia',
+                  into = c('sig_hierarquia', 'nome_hierarquia'),
+                  sep = '-') %>%
+  dplyr::mutate(nome_hierarquia = dplyr::case_when(
+    nome_hierarquia == ' Metrópole' ~ 'Metrópole',
+    nome_hierarquia == ' Capital Regional' ~ 'Capital Regional',
+    nome_hierarquia == ' Centro Sub' ~ 'Centro Sub',
+    nome_hierarquia == ' Centro de Zona' ~ 'Centro de Zona',
+    nome_hierarquia == ' Centro Local' ~ 'Centro Local'
+  )) %>%
+  tidyr::drop_na()
+
+nomes_municipios <- base %>%
+  dplyr::transmute(as.character(id_municipio), cidade) %>%
+  dplyr::rename('dmu' = `as.character(id_municipio)`)
+
+# -------------------------------------------------------------------------
+inputs <- base[,1:16]
+
+outputs <- base[,-c(5:16)]
+
+# -------------------------------------------------------------------------
+eficiencia_metropole <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                          sheet = 8) %>%
+  janitor::clean_names() %>%
+  dplyr::select(dmu, score) %>%
+  dplyr::mutate(nome_hierarquia = 'Metrópole')
+
+eficiencia_capital_regional <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                                 sheet = 11) %>%
+  janitor::clean_names() %>%
+  dplyr::select(dmu, score) %>%
+  dplyr::mutate(nome_hierarquia = 'Capital Regional')
+
+eficiencia_centro_sub <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                           sheet = 14) %>%
+  janitor::clean_names() %>%
+  dplyr::select(dmu, score) %>%
+  dplyr::mutate(nome_hierarquia = 'Centro Sub')
+
+eficiencia_centro_zona <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                            sheet = 17) %>%
+  janitor::clean_names() %>%
+  dplyr::select(dmu, score) %>%
+  dplyr::mutate(nome_hierarquia = 'Centro de Zona')
+
+eficiencia_centro_local <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                             sheet = 20) %>%
+  janitor::clean_names() %>%
+  dplyr::select(dmu, score) %>%
+  dplyr::mutate(nome_hierarquia = 'Centro Local')
+
+eficiencia <- dplyr::bind_rows(eficiencia_metropole,
+                               eficiencia_capital_regional,
+                               eficiencia_centro_sub,
+                               eficiencia_centro_zona,
+                               eficiencia_centro_local) %>%
+  dplyr::full_join(nomes_municipios, by = c('dmu' = 'dmu')) %>%
+  tidyr::drop_na()
+
+# -------------------------------------------------------------------------
+pesos_metropole <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                     sheet = 9,
+                                     range = 'B1:O212') %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(nome_hierarquia = 'Metrópole')
+
+pesos_capital_regional <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                            sheet = 12,
+                                            range = 'B1:O298') %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(nome_hierarquia = 'Capital Regional')
+
+pesos_centro_sub <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                      sheet = 15,
+                                      range = 'B1:O488') %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(nome_hierarquia = 'Centro Sub')
+
+pesos_centro_zona <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                       sheet = 18,
+                                       range = 'B1:O428') %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(nome_hierarquia = 'Centro de Zona')
+
+pesos_centro_local <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                                        sheet = 21,
+                                        range = 'B1:O3971') %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(nome_hierarquia = 'Centro Local')
+
+pesos <- dplyr::bind_rows(pesos_metropole,
+                          pesos_capital_regional,
+                          pesos_centro_sub,
+                          pesos_centro_zona,
+                          pesos_centro_local) %>%
+  dplyr::full_join(nomes_municipios, by = c('dmu' = 'dmu')) %>%
+  tidyr::drop_na()
+
+# -------------------------------------------------------------------------
+populacao <- sidrar::get_sidra(
+  api = '/t/4714/n6/all/v/93/p/all') %>%
+  janitor::clean_names() %>%
+  dplyr::select(municipio_codigo, valor)
+
+# -------------------------------------------------------------------------
+rm(eficiencia_metropole,
+   eficiencia_capital_regional,
+   eficiencia_centro_sub,
+   eficiencia_centro_zona,
+   eficiencia_centro_local,
+   pesos_metropole,
+   pesos_capital_regional,
+   pesos_centro_sub,
+   pesos_centro_zona,
+   pesos_centro_local,
+   nomes_municipios)
+
+
+# TRATAMENTO: -------------------------------------------------------------
+ref_pesos <- pesos %>%
+  dplyr::filter(nome_hierarquia == 'Centro Sub') %>%
+  dplyr::filter(score == 1) %>%
+  dplyr::select(dmu,
+                cidade,
+                weight_seguranca_publica,
+                weight_desporto_e_lazer,
+                weight_educacao_e_cultura,
+                weight_assistencia_social,
+                weight_saude)
+
+
+dplyr::slice_max()
+
+# VISUALIZAÇÃO: -----------------------------------------------------------
+eficiencia %>%
+  dplyr::filter(score < 1) %>%
+  ggplot() +
+  geom_boxplot(aes(x = nome_hierarquia, y = score))
+
+
+outputs[,5:20] %>%
+  tidyr::pivot_longer(cols = dplyr::everything(),
+                      names_to = 'output',
+                      values_to = 'valor') %>%
+  ggplot() +
+  geom_boxplot(aes(x = output, y = valor))
+
+
+inputs[,5:16] %>%
+  tidyr::pivot_longer(cols = dplyr::everything(),
+                      names_to = 'input',
+                      values_to = 'valor') %>%
+  ggplot() +
+  geom_boxplot(aes(y = input, x = valor))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/dea_ver_2.R
@@ -0,0 +1,151 @@
+# PACOTES: ----------------------------------------------------------------
+library(magrittr)
+library(ggplot2)
+
+
+
+
+# IMPORTAÇÃO E ARRUMAÇÃO: -------------------------------------------------
+base <- readxl::read_xlsx(path = './dados/240828_dea.xlsx',
+                          sheet = 2,
+                          range = 'A1:AE5395') %>%
+  janitor::clean_names() %>%
+  tidyr::separate(col = 'hierarquia',
+                  into = c('sig_hierarquia', 'nome_hierarquia'),
+                  sep = '-') %>%
+  dplyr::mutate(nome_hierarquia = dplyr::case_when(
+    nome_hierarquia == ' Metrópole' ~ 'Metrópole',
+    nome_hierarquia == ' Capital Regional' ~ 'Capital Regional',
+    nome_hierarquia == ' Centro Sub' ~ 'Centro Subregional',
+    nome_hierarquia == ' Centro de Zona' ~ 'Centro de Zona',
+    nome_hierarquia == ' Centro Local' ~ 'Centro Local'
+  )) %>%
+  tidyr::drop_na() %>%
+  dplyr::mutate(id_municipio = as.character(id_municipio))
+
+
+populacao <- sidrar::get_sidra(
+  api = '/t/4714/n6/all/v/93/p/all') %>%
+  janitor::clean_names() %>%
+  dplyr::select(municipio_codigo, valor)
+
+
+consolidado <-  base %>%
+  dplyr::full_join(populacao,
+                   by = c('id_municipio' = 'municipio_codigo')) %>%
+  dplyr::filter(valor >= 75000 & valor <= 150000) %>%
+  tidyr::drop_na() %>%
+  dplyr::mutate(outros = previdencia_social +
+                  legislativa +
+                  gestao_ambiental_agricultura +
+                  comercio_e_servicos +
+                  administracao +
+                  outros) %>%
+  dplyr::select(!c(previdencia_social,
+                     legislativa,
+                     gestao_ambiental_agricultura,
+                     comercio_e_servicos,
+                     administracao))
+
+
+consolidado <-  base %>%
+  dplyr::full_join(populacao,
+                   by = c('id_municipio' = 'municipio_codigo')) %>%
+  dplyr::filter(valor >= 75000 & valor <= 150000) %>%
+  dplyr::select(id_municipio,
+                cidade,
+                nome_hierarquia,
+                assistencia_social,
+                urbanismo_saneamento_transporte,
+                desporto_e_lazer,
+                educacao_e_cultura,
+                ods_1,
+                ods_2,
+                ods_3,
+                ods_4,
+                ods_6,
+                ods_10) %>%
+  tidyr::drop_na()
+
+
+
+
+# TRATAMENTO E MODELAGEM --------------------------------------------------
+referencia <- consolidado %>%
+  dplyr::select(id_municipio,
+                cidade)
+
+outputs <- consolidado %>%
+  dplyr::select(dplyr::starts_with('ods'))
+
+inputs <- consolidado %>%
+  dplyr::select(!c(id_municipio,
+                   cidade,
+                   nome_hierarquia,
+                   dplyr::starts_with('ods')))
+
+resultado <- rDEA::dea(
+  XREF = inputs,
+  YREF = outputs,
+  X = inputs,
+  Y = outputs,
+  model = 'input',
+  RTS = 'constant')
+
+eficiencia <- referencia %>%
+  dplyr::mutate(eficiencia = resultado$thetaOpt) %>%
+  dplyr::arrange(desc(eficiencia))
+
+
+# VISUALIZAÇÃO: -----------------------------------------------------------
+consolidado %>%
+  dplyr::select(dplyr::starts_with('ods')) %>%
+  tidyr::pivot_longer(cols = everything(),
+                      names_to = 'ods',
+                      values_to = 'valor') %>%
+  tidyr::separate(col = 'ods',
+                  into = c('ods_pre', 'ods_num'),
+                  sep = '_') %>%
+  dplyr::select(-ods_pre) %>%
+  dplyr::mutate(ods_num = as.numeric(ods_num)) %>%
+  ggplot() +
+  geom_boxplot(aes(x = as.factor(ods_num), y = valor)) +
+  labs(title = 'Dispersão das ODS (INPUTS)',
+       subtitle = 'Municípios de Tamanho Populacional entre 75.000 e 150.000',
+       x = 'Objetivo de Desenvolvimento Sustentável (ODS)',
+       y = 'Índice') +
+  theme_bw()
+
+
+consolidado %>%
+  dplyr::select(!c(id_municipio,
+                   cidade,
+                   sig_hierarquia,
+                   nome_hierarquia,
+                   valor,
+                   dplyr::starts_with('ods'))) %>%
+  tidyr::pivot_longer(cols = everything(),
+                      names_to = 'orcamento',
+                      values_to = 'valor') %>%
+  dplyr::mutate(orcamento = dplyr::case_when(
+    orcamento == 'urbanismo_saneamento_transporte' ~ 'Urbanismo, Saneamento e Transporte',
+    orcamento == 'saude' ~ 'Saúde',
+    orcamento == 'educacao_e_cultura' ~ 'Educação e Cultura',
+    orcamento == 'previdencia_social' ~ 'Previdência Social',
+    orcamento == 'assistencia_social' ~ 'Assistência Social',
+    orcamento == 'seguranca_publica' ~ 'Segurança Pública',
+    orcamento == 'outros' ~ 'Outros',
+    orcamento == 'legislativa' ~ 'Legislativa',
+    orcamento == 'gestao_ambiental_agricultura' ~ 'Gestão Ambiental e Agricultura',
+    orcamento == 'desporto_e_lazer' ~ 'Desporto e Lazer',
+    orcamento == 'comercio_e_servicos' ~ 'Comércio e Serviços',
+    orcamento == 'administracao' ~ 'Administração')) %>%
+  ggplot() +
+  geom_boxplot(aes(y = orcamento, x = valor)) +
+  labs(title = 'Dispersão dos Orçamentos (OUTPUTS)',
+       subtitle = 'Municípios de Tamanho Populacional entre 75.000 e 150.000',
+       x = 'Orçamento Per Capta (R$/Habitante)',
+       y = '') +
+  theme_bw()
+
+
