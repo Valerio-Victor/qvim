a8e60f06f21bc0373922d486f561877dc57dd1ec
valerio-victor <victor.dmv@unifei.edu.br>
2024-11-21 14:58:58 -0300

    Atualização do relatório.

---
+++ scripts/chagpt.R
@@ -0,0 +1,12 @@
+# Análises realizadas no chatgpt
+writexl::write_xlsx(x = mapa_insumos,
+                    path = "./dados/mapa_insumos.xlsx")
+
+writexl::write_xlsx(x = mapa_produtos,
+                    path = "./dados/mapa_produtos.xlsx")
+
+dados_idsc_01 <- readxl::read_xlsx(path = "./dados/idsc_2022.xlsx",
+                                   sheet = 1)
+
+dados_idsc_01 <- readxl::read_xlsx(path = "./dados/idsc_2022.xlsx",
+                                   sheet = 1)
